5ae31af6027514eee1fb2515ba985459c70144d8
M. Barber <36747278+vinyl704@users.noreply.github.com>
2025-06-01 13:46:34 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 const {
   DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
-
+const config = {
+  DB_SSL: fs.readFileSync("ca.pem")
+}
 module.exports = {
   development: {
     client: "postgresql",
@@ -21,8 +23,11 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    connection: {
+      connectionString:DATABASE_URL,
+      ssl:config['DB_SSL'] ? { rejectUnauthorized: false } : false,
+    },
+    pool: { min: 0, max: 10 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
