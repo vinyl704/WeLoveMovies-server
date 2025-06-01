0db2d473ad027004c8ece3cd08b13cef581c48ee
M. Barber <36747278+vinyl704@users.noreply.github.com>
2025-06-01 13:59:40 -0400

    Update server.js
    didn't update other PORT variables to use port, i did it this time

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const port = process.env.PORT || 5000;
 const app = require("./app");
 const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const listener = () => console.log(`Listening on Port ${port}!`);
 
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(PORT, listener);
+    app.listen(port, listener);
   })
   .catch(console.error);
